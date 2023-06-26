202c6ac67d020ce57bf2a8053590aca291339db1
USER <USER@DESKTOP-EI3H97L>
2023-06-26 23:39:37 +0900

    20230626

--- frontend/public/js/과제/과제3.js
+++ frontend/public/js/과제/과제3.js
@@ -5,19 +5,24 @@ let menu = menuinput.value
 let priceinput = document.querySelector(".priceinput")
 let price = Number(priceinput.value)
 
-let sum = 0;
 
+let count = 0;
+let sum = 0;
+let i = 0;
+
+let buttonHTML = `<tr><th>날짜</th><th>항목</th><th>금액</th><th>비고</th></tr>`
 
 function input(){
 	
-	for(let i = 0; i < table.rows.length; i++)  {
-    	sum += parseInt(table.rows[i].cells[2].innerHTML);
-	}
-	let buttonHTML = `<tr><th>날짜</th><th>항목</th><th>금액</th><th>비고</th></tr>`
-	buttonHTML += `<tr><td>${dateinput.value}</td><td>${menuinput.value}</td><td>${priceinput.value}</td><td><button class="btn btn-danger pull-right">삭제</button></td></tr>`
-	buttonHTML += `<tr><th colspan="2">-총합계-</th><th colspan="2">${sum}</th></tr>`
-	
+	sum += Number(priceinput.value)
+
+	buttonHTML[i] += `<tr class="cli"><td>${dateinput.value}</td><td>${menuinput.value}</td><td>${Number(priceinput.value)}</td><td><button onclick="inputdelete()" class="DeleteBtn" class="btn btn-danger pull-right">삭제</button></td></tr>`
+	i++;
+
 	document.querySelector(".출력구역").innerHTML = buttonHTML
+	
+	document.querySelector(".출력구역").innerHTML += `<tr><th colspan="2">-총합계-</th><th colspan="2">${sum.toLocaleString( )}</th></tr>`
+
 	
 	dateinput.value = ''
 	menuinput.value = ''
@@ -25,4 +30,9 @@ function input(){
 
 	
 }
+
+function inputdelete(){
+
+}
+
 

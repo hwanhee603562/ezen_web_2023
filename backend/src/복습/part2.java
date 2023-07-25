35a3bba72edb5e4cd12631ef5adb0307ef18fc74
504 <504@DESKTOP-BD9B2H9>
2023-07-25 10:39:16 +0900

    20230725

--- backend/src/복습/part2.java
+++ backend/src/복습/part2.java
@@ -17,6 +17,7 @@ public class part2 {
 		int 환타가격 = Integer.parseInt(재고관리.split("\n")[1].split(",")[2]);
 		int 사이다가격 = Integer.parseInt(재고관리.split("\n")[2].split(",")[2]);
 
+		int 주문번호 = 0;
 		
 		while(true) {
 			System.out.println("1.콜라[300] 2.환타[200] 3.사이다[100] 4.결제");
@@ -37,7 +38,6 @@ public class part2 {
 				} else {
 					환타재고--;
 					환타바구니++;
-					
 				}
 			} else if(ch == 3) {
 				if(사이다재고<0) {
@@ -66,6 +66,8 @@ public class part2 {
 					int ch3 = sc.nextInt();
 					if(ch3 >= sum) {
 						System.out.println("잔액 : "+(ch3-sum));	
+						주문번호++;
+						System.out.println("주문번호 : "+주문번호);
 						콜라바구니 =0;
 						환타바구니 =0;
 						사이다바구니 =0;
@@ -80,6 +82,7 @@ public class part2 {
 					}
 					
 				}else if(ch2 == 2) {
+					System.out.println("결제 취소되었습니다");
 					콜라재고 += 콜라바구니;
 					환타재고 += 환타바구니;
 					사이다재고 += 사이다바구니;

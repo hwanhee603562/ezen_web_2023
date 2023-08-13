a93e711a4d42b2343d2aee41d2937f2a793323bb
USER <USER@DESKTOP-EI3H97L>
2023-08-13 23:34:58 +0900

    복구

--- backend/src/과제/과제11/controller/BoardController.java
+++ backend/src/과제/과제11/controller/BoardController.java
@@ -1,86 +1,58 @@
 package 과제.과제11.controller;
 
+
 import java.util.ArrayList;
 
 import 과제.과제11.model.dao.BoardDao;
 import 과제.과제11.model.dto.BoardDto;
+import 과제.과제11.view.LoginPage;
 
 public class BoardController {
-	
+	// 0.싱글톤 
 	private static BoardController boardController = new BoardController();
-	public static BoardController getInstance() {
-		return boardController;
-	}
+	public static BoardController getInstance() { return boardController; }
 	private BoardController() {}
 	
-	// 9 boardWrite		게시물쓰기 페이지
-	public boolean boardWrite( String title, String content ) {
-		
-		// 1. 유효성 검사
-		if( title.length() == 0 || title.length() > 50) return false;
-		// 2. Dto [ 입력받은제목, 입력받은내용, 로그인 회원번호 ]
-		// 위 생성자를 Dto에 생성자를 새롭게 생성하였음
-			// 로그인 회원번호는 세션에 현재 로그인 중인 회원번호를 가져옴 
-		BoardDto boardDto = new BoardDto(title, content, MemberController.getInstance().getLoginSession());
-		
-		// 3. DAO에게 전달 후 결과를 받아 반환
+	// 9. boardWrite : 게시물쓰기 페이지 
+	public boolean boardWrite( String title , String content ) {
+		// 1. 유효성검사 
+		if( title.length() == 0 || title.length() > 50  ) { return false; }
+		// 2. Dto [ 입력받은제목 , 입력받은내용 , 로그인된회원번호= MemberController 의 loginSession  ]
+		BoardDto boardDto = new BoardDto( 
+							title, content, 
+							MemberController.getInstance().getLoginSession() );
+		// 2. 글쓰기전용 생성자가 없을때
+		// BoardDto boardDto2 = new BoardDto( 0, title, content, null , 0, MemberController.getInstance().getLoginSession(), null);
+		// 3. DAO에게 전달후 결과 받아서 받은 결과 바로 반환
 		return BoardDao.getInstance().boardWrite(boardDto);
 	}
-	// 10 boardPrint		모든 게시물 출력
+	// 10. boardPrint : 모든 게시물 출력 
 	public ArrayList<BoardDto> boardPrint() {
-
 		return BoardDao.getInstance().boardPrint();
 	}
-	// 11 boardView		개별 게시물 출력
-	public BoardDto boardView(int bno) {
-		
+	// 11. boardView : 개별 게시물 출력 
+	public BoardDto boardView( int bno ) {
 		return BoardDao.getInstance().boardView(bno);
-		
 	}
-	// 12 boardUpdate	게시물 수정
-		// bno		: 수정할 게시물의 식별[누구를 수정할 것인지]
-		// mno		: 작성요청회원과 작성자가 일치할 경우에만 수정 가능하도록 유효성검사
-		// title	: 수정할 제목
-		// content	: 수정할 내용
-	public int boardUpdate( int bno, int mno, String title, String content ) {
-		// 1. 유효성 검사
-			// 1 게시물의 작성자회원번호와 로그인된 회원의 번호와 일치하지 않으면
-		if( mno != MemberController.getInstance().getLoginSession() ) return 3;
-			// 2 제목 글자 수 체크
-		if( title.length()<1 || title.length()>50 ) return 4;
-
+	// 12. boardUpdate : 게시물 수정 [ 준비물 : bno = 수정할 게시물의 식별[누구를 수정할껀지] / mno(게시물의 작성자) =  수정요청한 회원과 게시물의 작성자 가 일치한 경우에만[유효성검사] / title : 수정할값 , content : 수정할값 ]
+	public int boardUpdate( int bno , int mno , String title , String content ) {
+		// 1. 유효성 검사 
+			// 1. 게시물의 작성자회원번호 와 로그인된 회원번호 과 일치 하지 않으면 
+		if( mno != MemberController.getInstance().getLoginSession() ) { return 3;}
+			// 2. 제목 글자 수 체크 
+		if( title.length() < 1 || title.length() > 50 ) { return 4; }
 		// 2. 
-		boolean result = BoardDao.getInstance().boardUpdate( new BoardDto(bno, title, content));
-		
+		boolean result = 
+				BoardDao.getInstance().boardUpdate( new BoardDto(bno, title, content) );
 		if( result ) return 1;
 		else return 2;
-		
 	}
-	// 13 boardDelete	게시물 삭제	
-		// bno		: 삭제할 게시물의 식별[누구를 삭제할 것인지]
-		// mno		: 삭제요청한 회원과 게시물의 작성자가 일치한 경우에만 삭제하도록 유효성검사
-	public int boardDelete(int bno, int mno) {
-		// 현재 로그인한 회원과 게시물 작성자 일치 여부확인
+	// 13. boardDelete : 게시물 삭제 [ 준비물 : bno = 삭제할 게시물의 식별[누구를 삭제할껀지] / mno(게시물의 작성자) = 삭제요청한 회원과 게시물의 작성자가 일치한 경우에만[유효성검사]
+	public int boardDelete( int bno , int mno  ) {
 		if( mno != MemberController.getInstance().getLoginSession() ) { return 3; }
-		
 		boolean result = BoardDao.getInstance().boardDelete( bno ) ;
 		if( result ) return 1;
 		else return 2;
-		
-
 	}
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- backend/src/과제/과제11/view/LoginPage.java
+++ backend/src/과제/과제11/view/LoginPage.java
@@ -3,234 +3,164 @@ package 과제.과제11.view;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-import java1.day13.Ex1.Board;
 import 과제.과제11.controller.BoardController;
 import 과제.과제11.controller.MemberController;
 import 과제.과제11.model.dto.BoardDto;
 import 과제.과제11.model.dto.MemberDto;
 
-// 로그인이 완료된 이후의 화면
 public class LoginPage {
-	
-	// 0 싱글톤
+	// 0.싱글톤 
 	private static LoginPage loginPage = new LoginPage();
-	public static LoginPage getInstance() {
-		return loginPage;
-	}
+	public static LoginPage getInstance() { return loginPage; }
 	private LoginPage() {}
+	// 0.입력객체 
+	private Scanner sc = new Scanner(System.in);
 	
-	// 0 입력객체
-	Scanner sc = new Scanner(System.in);
-	
-	// 1 loginMenu		로그인했을 때 메뉴 페이지
+	// 1. loginMenu	: 로그인했을때 메뉴 페이지 
 	public void loginMenu() {
 		
-		while(MemberController.getInstance().getLoginSession() != 0) {
+		while( MemberController.getInstance().getLoginSession() != 0 ) { // 로그인이 되어 있는 경우에만 메뉴 반복 
 			
-			// 모든 게시물 보기 실행
-			boardPrint();
+			boardPrint(); // 모든 글 조회
 			
-			System.out.println("\n\n======= 회원제 커뮤니티 =======");
-			System.out.print("1.로그아웃 2.회원정보 3.글쓰기 선택 4.글조회 선택>");
-			
+			System.out.println("\n\n ===== ===== ===== ===== ");
+			System.out.print("1.로그아웃 2.회원정보 3.글쓰기 4.글조회 선택 : ");
 			try {
 				
 				int ch = sc.nextInt();
-				// 만약에 타입과 일치하지 않는 값을 입력하면 
-				// java.util.InputMismatchException 발생
-				sc.nextLine();
-				if(ch==1) MemberController.getInstance().logOut();
+				if( ch == 1 ) { MemberController.getInstance().logOut(); }
+				if( ch == 2 ) { info(); }
+				if( ch == 3 ) { boardWrite(); }
+				if( ch == 4 ) { boardView(); }
 				
-				if(ch==2) info();
-				
-				if(ch==3) boardWrite();
-				
-				if(ch==4) boardView();
-
-			} catch(Exception e) {
-				System.out.println("경고] 잘못 입력했습니다"+e);
-				sc =  new Scanner(System.in);
-				// 입력객체의 메모리를 새롭게 만들어줘야함
-			}
-		}
-		
-		
-		
-	}
-	// 2 info			로그인된 회원정보 페이지
+			}catch( Exception e ) {
+				System.out.println("경고] 잘못된 입력입니다. ");
+				sc = new Scanner(System.in); 
+			}  // c end 
+		} // w end 
+	}// f end 
+	
+	// 6. info		: 로그인된 회원정보 페이지 
 	public void info() {
-		
-		System.out.println("\n\n===== info =====");
-		// 1. 컨트롤에게 회원정보 요청해서 로그인된 회원정보[DTO] 반환 받기
-			// 로그인된 후 식별키는 세션에 저장된 회원번호가 되므로  
-			// 매개변수가 없다
+		System.out.println("\n\n ===== info ===== ");
+		// 1. 컨트롤에게 회원정보 요청해서 1명의 회원정보[DTO] 반환 받기 
 		MemberDto result = MemberController.getInstance().info();
-		
-		/*
-		 	null값을 반환하여 해당 메서드를 호출한 곳으로 반환시킬 경우 에러 발생
-		 */
-		
-		// 2. 반환받은 회원정보객체[memberDto]의 각 회원정보를 출력 
-		System.out.println(">ID : "+ result.getMid());
-		System.out.println(">NAME : "+ result.getMname());
-		System.out.println(">PHONE : "+ result.getMphone());
-		
-		// 2. 서브메뉴
-		System.out.println("1.비밀번호수정 2.회원탈퇴 3.뒤로가기 선택 : ");
+		// 2. 반환받은 회원정보객체[memberDto] 의 각 필드를 출력 
+		// System.out.println(">ID : " + result.id ); // DTO 필드의 직접접근 불가능 
+		System.out.println(">ID : " + result.getMid() ); // getter or setter
+		System.out.println(">NAME : " + result.getMname() );
+		System.out.println(">PHONE : " + result.getMphone() );
+		// 2. 서브메뉴 
+		System.out.print("1.비밀번호수정 2.회원탈퇴 3.뒤로가기 선택 : ");
 		int ch = sc.nextInt();
-		if(ch==1) {
-			infoUpdate();
-		}
-		if(ch==2) {
-			infoDelete();
-		}
-		if(ch==3) {	// 생략가능
-			return;	
-		}
-		
-		
-		
-		
+		if( ch == 1 ) { infoUpdate(); }
+		if( ch == 2 ) { infoDelete(); }
+		if( ch == 3 ) { return; } // 생략가능 
 	}
-	// 3 infoUpdate		회원수정 페이지
+	
+	// 7. infoUpdate : 비밀번호수정 페이지 
 	public void infoUpdate() {
-		
-		System.out.println("\n\n===== password Update =====");
-		System.out.println("새로운 비밀번호 : ");
+		System.out.println("\n\n ===== password Update ===== ");
+		System.out.print("새로운 비밀번호 : ");	
 		String newPw = sc.next();
 		
-		boolean result = MemberController.getInstance().infoUpdate(newPw);
-		if(result) {
-			System.out.println("안내] 비밀번호수정완료 : 로그아웃입니다");
+		boolean result 
+			= MemberController.getInstance().infoUpdate(newPw);
+		if( result ) { 
+			System.out.println("안내]비밀번호수정완료 : 로그아웃됩니다."); 
 			MemberController.getInstance().logOut();
-		} else {
-			System.out.println("경고] 비밀번호수정실패 : 관리자에게 문의");
 		}
-		
-		
-	}
-	// 4 infoDelete		회원탈퇴 페이지
+		else { System.out.println("경고]비밀번호수정실패 : 관리자에게문의");}
+	} // f end 
+	
+	// 8. infoDelete : 회원탈퇴 페이지 
 	public void infoDelete() {
-		
-		System.out.println("\n\n===== password Delete =====");
-		System.out.println("정말 탈퇴하시겠습니까? 1.예 2.아니오 : ");
+		System.out.println("\n\n ===== member delete ===== ");
+		System.out.print("정말 탈퇴하시겠습니까?? 1.예 2.아니요 : "); 
 		int ch = sc.nextInt();
-		if( ch == 1 ) {
-			boolean result = MemberController.getInstance().infoDelete();
-			if( result ) {
-				System.out.println("안내] 회원탈퇴성공 : 로그아웃됩니다");
+		if( ch == 1 ) { 
+			boolean result 
+				= MemberController.getInstance().infoDelete(); 
+			if( result ) { 
+				System.out.println("안내]회원탈퇴성공 : 로그아웃됩니다.");
 				MemberController.getInstance().logOut();
-			} else {
-				System.out.println("경고] 회원탈퇴실패 : 관리자에게 문의");
+			}else {
+				System.out.println("경고]회원탈퇴실패 : 관리자에게문의");
 			}
 		}
-		
-	}
+	} // f end 
 	
-	
-	// 9 boardWrite		게시물쓰기 페이지
-	public void boardWrite(  ) {
-		System.out.println("----- post write -----");
-
-		System.out.println("제목 > ");
-		String title = sc.next();
-		System.out.println("내용 > ");
-		String content = sc.next();
-
-		boolean result =
-		BoardController.getInstance().boardWrite(title, content);
-		
-		if(result) {
-			System.out.println("안내] 글쓰기 등록");
-		} else {
-			System.out.println("안내] 글쓰기 실패 : 제목 필수 입력");
-		}	
+	// 9. boardWrite : 게시물쓰기 페이지 
+	public void boardWrite() {
+		System.out.println(" ----- post write ----- ");
+		sc.nextLine();
+		System.out.print("제목 > "); 		String title = sc.nextLine();
+		System.out.print("내용 > "); 		String content = sc.nextLine();
+		boolean result = 
+				BoardController.getInstance().boardWrite(title, content);
+		if( result ) { System.out.println("안내] 글쓰기 등록 "); }
+		else {System.out.println("안내] 글쓰기 실패 : 제목 1~50 사이로 필수 입력"); }
 	}
-	// 10 boardPrint		모든 게시물 출력
+	// 10. boardPrint : 모든 게시물 출력 
 	public void boardPrint() {
-		System.out.println("----- post LIST -----");
-		// 1. 여러 개의 게시물을 요청해서 반환된 결과 저장
-		ArrayList<BoardDto> result = 
-			BoardController.getInstance().boardPrint();
-		
-		// 2. 출력
-		System.out.printf("%-3s %-4s %-15s %-10s %s \n", "no", "view", "date", "mid", "title");
-		for(int i=0; i<result.size(); i++) {
-			BoardDto dto = result.get(i);
-			
-			System.out.printf("%-3s %-4s %-15s %-10s %s \n", dto.getBno(), dto.getBview(), dto.getBdate(), dto.getMid(), dto.getBtitle());
+		System.out.println(" ----- post LIST ----- ");
+		// 1. 여러개의 게시물을 요청해서 반환된 결과 저장 
+		ArrayList<BoardDto> result = BoardController.getInstance().boardPrint();
+		// 2. 출력 
+		System.out.printf("%-3s %-4s %-19s %-10s %s \n" , "no" , "view" , "date" , "mid" , "title");
+		for( int i = 0 ; i<result.size(); i++ ) {
+			BoardDto dto = result.get(i);	// i번째의 객체를 호출 
+			System.out.printf("%-3s %-4s %-19s %-10s %s \n" , 
+					dto.getBno() , dto.getBview() , dto.getBdate() , dto.getMid() , dto.getBtitle() );
 		}
 	}
-	// 11 boardView		개별 게시물 출력(회원정보 출력과 방식은 동일)
+	// 11. boardView : 개별 게시물 출력
 	public void boardView() {
-		System.out.println("----- post VIEW -----");
-		
-		// 1 보고자하는 게시물의 게시물번호를 입력받기 [식별번호]
-		System.out.println("게시물번호 : ");
-		int bno = sc.nextInt();
-		sc.nextLine();
-		// 2
+		System.out.println(" ----- post VIEW ----- ");
+		// 1. 보고자하는 게시물의 게시물번호를 입력받기[ 식별번호] 
+		System.out.print("게시물번호 : "); int bno = sc.nextInt();
+		// 2. 
 		BoardDto result = BoardController.getInstance().boardView(bno);
-		// 3 출력
-		System.out.printf("bno : %-3s view : %-3s mid : %-10s date : %-19s \n",
-				result.getBno(), result.getBview(), result.getMid(), result.getBdate());
-		System.out.printf("title : %s, \n", result.getBtitle());
-		System.out.printf("content : %s, \n", result.getBcontent());
+		// 3. 출력 
+		System.out.printf("bno : %-3s view : %-3s mid : %-10s date : %-19s \n" , 
+						result.getBno() , result.getBview() , 
+						result.getMid() , result.getBdate() );
+		System.out.printf("title : %s \n" , result.getBtitle() );
+		System.out.printf("content : %s \n " , result.getBcontent() );
 		
-		System.out.println("1.뒤로가기 2.수정 3.삭제 선택>");
+		// 4. 추가메뉴 
+		System.out.print("1.뒤로가기 2.수정 3.삭제 선택> "); 
 		int ch = sc.nextInt();
+		if( ch == 1 ) {}
+		if( ch == 2 ) { boardUpdate( bno , result.getMno() ); } // 보고 있는 게시물 번호와 작성자회원번호
+		if( ch == 3 ) { boardDelete( bno , result.getMno() ); }
+	}
+	// 12. boardUpdate : 게시물 수정 [ 게시물번호 식별해서 제목이랑 내용만 수정 -> 로그인된 사람과 작성자가 일치할경우 가능하도록  ]
+	public void boardUpdate( int bno , int mno ) {
+		System.out.println(" ----- post update ----- ");
 		sc.nextLine();
-		
-		if(ch == 1) {}
-		if(ch == 2) boardUpdate( bno, result.getMno() );
-		if(ch == 3) boardDelete( bno, result.getMno() );
-		
+		System.out.print("수정할 제목 > "); 		String title = sc.nextLine();
+		System.out.print("수정할 내용 > "); 		String content = sc.nextLine();
+		int result = BoardController.getInstance().boardUpdate(  bno ,  mno ,  title ,  content );
+		if( result == 1 ) {System.out.println("안내] 글 수정 성공 ");}
+		else if( result == 2 ) { System.out.println("안내] 글 수정 실패 : 관리자 오류 ");}
+		else if( result == 3 ) { System.err.println("경고] 본인 글만 수정 가능 합니다. ");}
+		else if( result == 4 ) { System.err.println("경고] 수정할 제목을 1~50글자 사이로 입력해주세요.");}
 	}
-	// 12 boardUpdate	게시물 수정 
-	// [ 게시물번호 식별해서 제목과 내용만 수정 -> 로그인된 사람과 작성자가 일치할 경우 가능 ]
-	public void boardUpdate( int bno, int mno ) {
-		
-		System.out.println("----- post write -----");
-
-		// 1 수정할 내용 입력
-		System.out.println("수정할 제목 > ");
-		String title = sc.nextLine();
-		System.out.println("수정할 내용 > ");
-		String content = sc.nextLine();
-		
-		// 2 
-		int result = BoardController.getInstance().boardUpdate(bno, mno, title, content);
-		
-		
-		if( result == 1 ) System.out.println("안내] 글 수정 성공");
-		else if(result == 2 ) System.out.println("안내] 글 수정 실패 : 관리자 오류");
-		else if(result == 3 ) System.out.println("안내] 본인 글만 수정 가능 합니다");
-		else if(result == 4 ) System.out.println("안내] 수정할 제목을 1~50글자 사이로 입력해주세요");
-		
-
+	// 13. boardDelete : 게시물 삭제 [ 게시물번호 식별해서 삭제 -> 로그인된 사람과 작성자가 일치할경우 가능하도록 ]
+	public void boardDelete( int bno , int mno ) {
+		int result = BoardController.getInstance().boardDelete( bno , mno );
+		if( result == 1) {System.out.println("안내] 글 삭제 성공");}
+		else if( result == 2 ) { System.out.println("안내] 글 삭제 실패 : 관리자 오류 ");}
+		else if( result == 3 ) { System.err.println("경고] 본인 글만 삭제 가능 합니다. ");}
 	}
-	// 13 boardDelete	게시물 삭제
-	// [ 게시물번호 식별해서 삭제 -> 로그인된 사람과 작성자가 일치할 경우 가능 ]
-	public void boardDelete( int bno, int mno ) {
-		
-		int result = BoardController.getInstance().boardDelete(bno , mno);
-		
-		if(result == 1) System.out.println("안내] 글 삭제 성공");
-		else if(result == 2) System.out.println("안내] 글 삭제 실패 : 관리자 오류");
-		else if(result == 3) System.out.println("경고] 본인 글만 삭제 가능합니다");
-		
-		
-		
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	
 }
+
+/*
+ 	ArrayList<리스트에저장할타입> 리스트 객체명 = new ArrayList<>();
+ 		1. .size()		: 리스트내 객체수 					==유사 result.length
+ 		2. .get( 인덱스 )	: 리스트내 인덱스번째의 객체 호출 		==유사 result[i]
+ 		3. .add( 객체 )	: 리스트내 객체 추가
+  
+ */

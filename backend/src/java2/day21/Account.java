3070b44f3297aad42bb2e8329b7e3ae9b8342c2d
504 <504@DESKTOP-BD9B2H9>
2023-08-16 13:14:37 +0900

    11

---
+++ backend/src/java2/day21/Account.java
@@ -0,0 +1,32 @@
+package java2.day21;
+
+public class Account {
+	
+	// 필드
+	private long balance;	// 잔고
+	
+	// 생성자
+	public Account() {
+		
+	}
+
+	// 메서드
+	public long getBalance() {
+		return balance;
+	}
+	
+		// 예금 메서드
+	public void deposit(int money) {
+		balance += money;
+	}
+		// 출금 메서드
+	public void withdraw(int money) throws 잔고부족예외 {
+		// 예금 부족하면 출금 금지
+		if( this.balance < money ) {
+			// 고의적으로 조건에 해당될 시 예외발생
+			 throw new 잔고부족예외("잔고부족 : "+(money-this.balance)+" 만큼 부족");
+		}
+		this.balance -= money;
+	}
+	
+}
---
+++ backend/src/java2/day21/잔고부족예외.java
@@ -0,0 +1,19 @@
+package java2.day21;
+
+public class 잔고부족예외 extends Exception {
+
+	// 기본생성자
+	public 잔고부족예외() {
+		
+	}
+	
+	// 매개변수1개 생성자 : 예외 정보를 받기 위한 매개변수
+	public 잔고부족예외 (String message) {
+		super(message);
+	}
+	
+	
+	
+	
+	
+}
